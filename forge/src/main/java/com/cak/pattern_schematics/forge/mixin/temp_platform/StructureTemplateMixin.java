b7717e0c35f848f77d4fb5d7f31180e9b53729de
cake <cakeistastydev@gmail.com>
2025-04-27 11:48:16 +0100

    thing

--- forge/src/main/java/com/cak/pattern_schematics/forge/mixin/temp_platform/StructureTemplateMixin.java
+++ forge/src/main/java/com/cak/pattern_schematics/forge/mixin/temp_platform/StructureTemplateMixin.java
@@ -10,9 +10,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(StructureTemplate.class)
 public class StructureTemplateMixin {
-
   
-  @Redirect(method = "placeInWorld", at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/world/level/ServerLevelAccessor;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z", remap = true))
+  @Redirect(method = "placeInWorld", at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/world/level/ServerLevelAccessor;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"))
   private boolean setBlock(ServerLevelAccessor instance, BlockPos pos, BlockState state, int i) {
 //    if (instance instanceof PatternSchematicWorld patternSchematicWorld)
 //      return patternSchematicWorld.setCloneBlock(pos, state, i);
